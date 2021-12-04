e81bdec2e863268dcf85f8f9579fd2f442ad1ac4
GG Bond <email@sample.com>
2021-12-04 22:51:20 +0800

    ADD:leetcode383

---
+++ hash/leetcode383/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func canConstruct(ransomNote string, magazine string) bool {
+	hash := make([]int, 26)
+	for _, v := range magazine {
+		hash[v-'a'] = hash[v-'a'] + 1
+	}
+
+	for _, v := range ransomNote {
+		hash[v-'a'] = hash[v-'a'] - 1
+		if hash[v-'a'] < 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(canConstruct("aa", "aab"))
+}
